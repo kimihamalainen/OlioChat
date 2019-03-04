b65beb9160645877b8a550a655ed6f9dd7f83fff
Mikohakkinen <47979165+Mikohakkinen@users.noreply.github.com>
2019-03-04 15:21:36 +0200

    Add files via upload

--- Chat/view1_chat.php
+++ Chat/view1_chat.php
@@ -1,28 +1,30 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
   <head>
-    <title>Chat</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Tsätti</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
   </head>
   <body>
+  <section>
+    <h1>Chat</h1>
+    <h2>Kirjoita uusi viesti ja <br> paina Lähetä-nappia</h2>
     <form action="?toiminto=lahetys" method="post">
       <input type="text" name="viesti">
-      <input type="submit" value="Send">
+      <input type="submit" value="Lähetä">
     </form>
-  <div id="chatbox">
-    <?php
-    // tämä on käyttöliittymä  ja näkymä tietoihin 	
-      if (empty($this->viestit)) {
-          echo "Chatissa ei ole viestejä.";
-      } else {
-      $lkm=0;    
-      foreach ($this->viestit as $viesti) {
-            $lkm=$lkm+1; // laskee rivien määrän määrän   
-        echo " " . htmlspecialchars($viesti) . "<br>";
-        include("model2_lkm.php"); // model2 -rajoittaa rivien määrää 		
-          }
-      }
-      ?>
-  </div>
+    </section>
+	<?php
+	// tämä on käyttöliittymä  ja näkymä tietoihin 	
+    if (empty($this->viestit)) {
+        echo "Chatissa ei ole viestejä.";
+    } else {
+		$lkm=0;    
+		foreach ($this->viestit as $viesti) {
+        	$lkm=$lkm+1; // laskee rivien määrän määrän   
+			echo " " . htmlspecialchars($viesti) . "<br>";
+			include("model2_lkm.php"); // model2 -rajoittaa rivien määrää 		
+        }
+    }
+    ?>
   </body>
 </html>
