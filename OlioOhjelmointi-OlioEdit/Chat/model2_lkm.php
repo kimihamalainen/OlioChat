0cf87880ec176719ddb76da190e90985e613116c
kimi hämäläinen <39537519+kimihamalainen@users.noreply.github.com>
2019-03-11 15:22:45 +0200

    Delete model2_lkm.php

--- OlioOhjelmointi-OlioEdit/Chat/model2_lkm.php
+++
@@ -1,8 +0,0 @@
-<?php
-		# Tutkitaan viestien määrä lkm-muuttujasta
-			if ($lkm > 11) /* jos suurempi kuin 10 niin */
-			{
-			file_put_contents("data1_chat.txt", "");
-			/* koska file_put_contents ei lopu appen -määritykseen niin kaikki tiedot korvautuvat. */
-			}
-?>
